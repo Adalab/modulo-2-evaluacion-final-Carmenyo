60fa05f830956f577570f309b4bb0bf63b358b11
Carmenyo <carmencastroyo@gmail.com>
2022-04-07 23:10:58 +0200

    inputnamecoctel working

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,17 @@
 
 const searchButton = document.querySelector (".js-searchButton");
 const inputNameCoctel = document.querySelector (".js-searchInput");
-const resultList = document.querySelector ("js-main-list");
+const resultList = document.querySelector (".js-main-list");
 
+let allDataDrinks= []
 
 // Funciones
 function connectApi() {
-	fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+	let inputValue = inputNameCoctel.value.toLowerCase();
+
+	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`)
     .then((response)=> response.json())
     .then((data) => {
-      // printDrinks(data);
 			console.log(data.drinks);
     })
 }
