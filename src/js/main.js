5cf7125577f77fdd0f8ca168ed84e1ff6d959f14
Carmenyo <carmencastroyo@gmail.com>
2022-04-07 21:53:02 +0200

    fetch working

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
-'use strict';
+/* eslint-disable quotes */
+"use strict";
 
-console.log('>> Ready :)');
+const searchButton = document.querySelector (".js-searchButton");
+const inputNameCoctel = document.querySelector (".js-searchInput");
+const resultList = document.querySelector ("js-main-list");
+
+
+// Funciones
+function connectApi() {
+	fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then((response)=> response.json())
+    .then((data) => {
+      // printDrinks(data);
+			console.log(data.drinks);
+    })
+}
+//Al pulsar buscar conectar la API
+function handleClick(){
+  connectApi();
+}
+// //Evento Buscar
+searchButton.addEventListener("click", handleClick);
