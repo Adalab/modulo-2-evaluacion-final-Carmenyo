c2f1f15c75fc7616135696615011f04b17517379
Carmenyo <carmencastroyo@gmail.com>
2022-04-08 10:26:23 +0200

    main-list working

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,40 @@
 "use strict";
 
 const searchButton = document.querySelector (".js-searchButton");
-const inputNameCoctel = document.querySelector (".js-searchInput");
+const inputNameCocktail = document.querySelector (".js-searchInput");
 const resultList = document.querySelector (".js-main-list");
+const favoriteList = document.querySelector(".js-main-listfav")
+const defaultImage = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+const reset = document.querySelector (".js-formReset")
 
-let allDataDrinks= []
+let drinks = []
 
 // Funciones
 function connectApi() {
-	let inputValue = inputNameCoctel.value.toLowerCase();
+	let inputValue = inputNameCocktail.value.toLowerCase();
 
 	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`)
     .then((response)=> response.json())
     .then((data) => {
 			console.log(data.drinks);
-    })
+			drinks = data.drinks;
+      
+      let html = ""
+        for (const drinkItem of drinks){
+          
+          html += `
+            <li>
+            <h2>${drinkItem.strDrink}</h2>
+            <img src="${drinkItem.strDrinkThumb}"/>
+            </li>
+          `;
+    };
+      resultList.innerHTML = html;
+  })
+
 }
-//Al pulsar buscar conectar la API
 function handleClick(){
   connectApi();
 }
-// //Evento Buscar
+
 searchButton.addEventListener("click", handleClick);
