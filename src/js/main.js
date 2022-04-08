b5570118c7c8a8868fb6e2adec20280a03ef2bba
Carmenyo <carmencastroyo@gmail.com>
2022-04-08 10:36:45 +0200

    paintDrinks Function

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,44 @@
 /* eslint-disable quotes */
 "use strict";
 
-const searchButton = document.querySelector (".js-searchButton");
-const inputNameCocktail = document.querySelector (".js-searchInput");
-const resultList = document.querySelector (".js-main-list");
-const favoriteList = document.querySelector(".js-main-listfav")
-const defaultImage = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
-const reset = document.querySelector (".js-formReset")
+const searchButton = document.querySelector(".js-searchButton");
+const inputNameCocktail = document.querySelector(".js-searchInput");
+const resultList = document.querySelector(".js-main-list");
+const favoriteList = document.querySelector(".js-main-listfav");
+const defaultImage =
+  "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+const reset = document.querySelector(".js-formReset");
 
-let drinks = []
+let drinks = [];
 
 // Funciones
 function connectApi() {
-	let inputValue = inputNameCocktail.value.toLowerCase();
+  let inputValue = inputNameCocktail.value.toLowerCase();
 
-	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`)
-    .then((response)=> response.json())
+  function paintDrinks() {
+    let html = "";
+    for (const drinkItem of drinks) {
+      html += `
+      <li>
+      <h2>${drinkItem.strDrink}</h2>
+      <img src="${drinkItem.strDrinkThumb}"/>
+      </li>
+    `;
+    }
+    resultList.innerHTML = html;
+  }
+
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`
+  )
+    .then((response) => response.json())
     .then((data) => {
-			console.log(data.drinks);
-			drinks = data.drinks;
-      
-      let html = ""
-        for (const drinkItem of drinks){
-          
-          html += `
-            <li>
-            <h2>${drinkItem.strDrink}</h2>
-            <img src="${drinkItem.strDrinkThumb}"/>
-            </li>
-          `;
-    };
-      resultList.innerHTML = html;
-  })
-
+      console.log(data.drinks);
+      drinks = data.drinks;
+      paintDrinks();
+    });
 }
-function handleClick(){
+function handleClick() {
   connectApi();
 }
 
